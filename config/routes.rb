055cb2da9ee8525629c3e7e71de9638cc6ec633d
morimuraryo <108705136+morimuraryo@users.noreply.github.com>
2022-10-17 11:47:56 +0000

    [Finish]

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root :to =>"homes#top"
+  get "home/about"=>"homes#about"
+
+  get "search" => "searches#search"
+
+  devise_for :users
+
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+  get :followers, on: :member
+  get :follows, on: :member
+
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+end
